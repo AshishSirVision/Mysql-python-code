772ae06d7165c2f81983e95ae2e7d8634c3d2f8d
AshishSirVision <92631803+AshishSirVision@users.noreply.github.com>
2022-07-25 09:22:57 +0530

    pattern & matrix

---
+++ Pattern.py
@@ -0,0 +1,99 @@
+for i in range(1,6):
+    for j in range(1,6):
+        print(i,end=" ")#to print characters
+    print()#for new line
+    
+#2nd pattern
+print("\n---------------\n")
+
+for i in range(1,6):
+    for j in range(1,6):
+        print(j,end=" ")#to print characters
+    print()#for new line
+
+#2nd pattern
+print("\n---------------\n")
+
+for i in range(1,6):
+    for j in range(1,6):
+        print(" @ ",end=" ")#to print characters
+    print()#for new line
+#4th pattern
+print("\n---------------\n")
+
+for i in range(1,6):
+    for j in range(1,6):
+        print(i*j,end=" ")#to print characters
+    print()#for new line
+
+#4th pattern
+print("\n---------------\n")
+
+for i in range(1,6):
+    for j in range(1,6):
+        print(j%2,end=" ")#to print characters
+    print()#for new line
+
+#4th pattern
+print("\n---------------\n")
+
+for i in range(1,6):
+    for j in range(1,6):
+        print(j**3,end=" ")#to print characters
+    print()#for new line
+
+#4th pattern
+print("\n---------------\n")
+
+for i in range(1,6):
+    for j in range(1,6):
+        print(int(j==i),end=" ")#to print characters
+    print()#for new line
+
+#4th pattern
+print("\n---------------\n")
+
+for i in range(1,6):
+    for j in range(1,6):
+        print(int(j!=i),end=" ")#to print characters
+    print()#for new line
+
+#4th pattern
+print("\n---------------\n")
+
+for i in range(1,6):
+    for j in range(1,6):
+        print(int(j>=i),end=" ")#to print characters
+    print()#for new line
+
+#4th pattern
+print("\n---------------\n")
+
+for i in range(1,6):
+    for j in range(1,6):
+        print(ord("z"),end=" ")#to print characters
+    print()#for new line
+    
+#4th pattern
+print("\n---------------\n")
+
+for i in range(65,71):
+    for j in range(97,102):
+        print(chr(j),end=" ")#to print characters
+    print()#for new line
+
+#4th pattern
+print("\n---------------\n")
+
+for i in range(65,72):
+    for j in range(97,102):
+        print(ord(chr(i)),end=" ")#to print characters
+    print()#for new line
+
+#4th pattern
+print("\n---------------\n")
+
+for i in range(1,6):
+    for j in range(97,102):
+        print(ord(str(i)),end=" ")#to print characters
+    print()#for new line
---
+++ matrix1.py
@@ -0,0 +1,10 @@
+matrix1=[
+    [1,2,3,4],
+    [3,2,3,6,2,3],    
+    [3,5,5,6]
+    ]
+
+for i in range(0,len(matrix1)):
+    for j in range(0,len(matrix1[i])):
+        print(matrix1[i][j],end=" ")#to print characters
+    print()#for new line
